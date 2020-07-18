79d6136a7e6330cf756d93e0a641e82e8e6f0653
yawmiej <yomlateef@yahoo.com>
2020-07-18 10:20:02 +0100

    Include a file saving system

--- counter/lib/pagehit.js
+++ counter/lib/pagehit.js
@@ -1,8 +1,27 @@
-const httpReferrer = require('./httpreferrer');
+const httpReferrer = require('./httpreferrer'),
+  fileUtil = require('./fileutil'),
+  //default data folder
+  path = require('path'),
+  dataFolder = path.resolve('./data') + path.sep,
+  //file save throttling - no more than every 10 seconds
+  saveFrequency = 10 * 1000;
 
 module.exports = class {
   constructor() {
     this.counter = {};
+
+    (async () => {
+      //store folder available?
+      this.folder = await fileUtil.folderUsable(dataFolder);
+      if (!this.folder) return;
+
+      this.saved = await fileUtil.folderList(this.folder.path, '.json');
+
+      //import and merge latest data
+      if (this.saved.length) {
+        Object.assign(this.counter, require(this.saved[0].path));
+      }
+    })();
   }
 
   count(req) {
@@ -11,6 +30,32 @@ module.exports = class {
 
     this.counter[hash] = this.counter[hash] || 0;
 
+    // save event
+    this.saveTimer =
+      this.saveTimer || setTimeout(this.save.bind(this), saveFrequency);
+
     return ++this.counter[hash];
+  }
+
+  async save() {
+    //can save
+    if (!this.folder) return;
+
+    //save new file
+    let fn = `${this.folder.path}hit${+new Date()}.json`;
+
+    if (await fileUtil.write(fn, JSON.stringify(this.counter))) {
+      console.log(`page hits stored: ${fn}`);
+
+      //delete old files
+      fileUtil.unlinkMany(this.saved);
+
+      //add current file to saved list
+
+      this.saved = [{ path: fn }];
+    }
+
+    //reset timer
+    this.saveTimer = null;
   }
 };
