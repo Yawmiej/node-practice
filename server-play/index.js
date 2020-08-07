145a3deb6c6870589eef4023910af4c53d3ddc8c
yawmiej <yomlateef@yahoo.com>
2020-08-07 10:17:48 +0100

    Separated functions into modules

--- server-play/index.js
+++ server-play/index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplates');
 
 const product = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
 const overview = fs.readFileSync(
@@ -10,24 +11,6 @@ const overview = fs.readFileSync(
 const card = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
 const data = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
-
-const replaceTemplate = (template = '', product) => {
-  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITIY%}/g, product.quantity);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%FROM%}/g, product.from);
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  }
-
-  return output;
-};
 
 const server = http.createServer((req, res) => {
   const pathname = req.url;
