33ac01f05fe725ca5e601f54a48538bdc9e652b4
yawmiej <yomlateef@yahoo.com>
2020-08-08 12:06:08 +0100

    Created a post requst

--- starter/app.js
+++ starter/app.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'This is a new Dawn', app: 'Natours' });
+//Middleware to allow request body data
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: tours,
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const postId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: postId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: { tour: newTour } });
+    }
+  );
 });
 
 const port = 4000;
